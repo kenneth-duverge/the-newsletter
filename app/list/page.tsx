fefb7b5442588f3be62c922fc341af6985aebc28
Kenneth Duverge <kenneth.duverge@icloud.com>
2024-01-23 16:21:25 -0600

    Downgrade convex

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,28 +1,9 @@
-import { NewsLetterEvent } from '@/components/newsletter-event';
-import { api } from '@/convex/_generated/api';
-import { formatTime } from '@/lib/format-time';
-import { fetchQuery } from 'convex/nextjs';
+import { ListView } from '@/components/views/list';
 
 export default async function List() {
-  const events = await fetchQuery(api.events.getEvents);
-
   return (
     <>
-      {events.map((n) => {
-        return (
-          <NewsLetterEvent key={n._id} name={n.name}>
-            <NewsLetterEvent.Location
-              city={`${n.location.city}, ${n.location.state}`}
-              date={new Date(n.date).toLocaleDateString('en-us', {
-                weekday: 'long',
-                month: 'long',
-                day: 'numeric',
-              })}
-              time={formatTime(n.time)}
-            />
-          </NewsLetterEvent>
-        );
-      })}
+      <ListView />
     </>
   );
 }
