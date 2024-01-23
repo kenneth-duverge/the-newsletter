709e811bef5413fc016314d8f89397a33070db24
Kenneth Duverge <kenneth.duverge@icloud.com>
2024-01-23 16:12:39 -0600

    Clean up view picker buttons

--- components/view-picker.tsx
+++ components/view-picker.tsx
@@ -6,14 +6,26 @@ import { usePathname } from 'next/navigation';
 
 import { Button } from './ui/button';
 
+interface LinkButtonProps {
+  className?: string;
+  href: string;
+  active: boolean;
+}
+
 const LinkButton = ({
   children,
-  to,
-  className,
-}: React.PropsWithChildren<{ className: string; to: 'calendar' | 'list' | 'blog' }>) => {
+  href,
+  className = '',
+  active,
+}: React.PropsWithChildren<LinkButtonProps>) => {
   return (
-    <Button asChild variant="outline" size="icon" className={className}>
-      <Link href={`/${to}`}>{children}</Link>
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className={`${className} ${active ? 'bg-accent text-accent-foreground' : ''}`}
+    >
+      <Link href={href}>{children}</Link>
     </Button>
   );
 };
@@ -21,27 +33,16 @@ const LinkButton = ({
 export const ViewPicker = () => {
   const pathname = usePathname();
   return (
-    <div className="flex gap-2 w-fit p-4">
-      <LinkButton
-        to="calendar"
-        className={
-          pathname.includes('calendar') || pathname === '' ? 'bg-accent text-accent-foreground' : ''
-        }
-      >
+    <nav className="flex gap-2 w-fit p-4">
+      <LinkButton href="/calendar" active={pathname.includes('calendar')}>
         <CalendarDays />
       </LinkButton>
-      <LinkButton
-        to="list"
-        className={pathname.includes('list') ? 'bg-accent text-accent-foreground' : ''}
-      >
+      <LinkButton href="/list" active={pathname.includes('list')}>
         <List />
       </LinkButton>
-      <LinkButton
-        to="blog"
-        className={pathname.includes('blog') ? 'bg-accent text-accent-foreground' : ''}
-      >
+      <LinkButton href="/blog" active={pathname.includes('blog')}>
         <BookOpenText />
       </LinkButton>
-    </div>
+    </nav>
   );
 };
